feafc7bd1c5af6d1eab4ace7ae7f8f2f9b9343d3
ioannis <yannoj2@gmail.com>
2025-05-16 19:27:57 -0400

    Add modal open/close functionality

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,28 @@ const initialCards = [
   },
 ];
 
+const editProfileButton = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+
+editProfileButton.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-opened");
+});
+
+editProfileCloseBtn.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-opened");
+});
+
+const newPostButton = document.querySelector(".profile__add-btn");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+
+newPostButton.addEventListener("click", function () {
+  newPostModal.classList.add("modal_is-opened");
+});
+
+newPostCloseBtn.addEventListener("click", function () {
+  newPostModal.classList.remove("modal_is-opened");
+});
+
 console.log(initialCards);
